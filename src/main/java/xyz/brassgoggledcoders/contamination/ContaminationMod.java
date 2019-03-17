2791803691048bf78bf55f40bf0cf0892c6e4209
warlordjones <warlordjones@gmail.com>
2019-03-17 22:42:47 +0000

    Don't run interacter logic twice
    //TODO Typed interacters

--- src/main/java/xyz/brassgoggledcoders/contamination/ContaminationMod.java
+++ src/main/java/xyz/brassgoggledcoders/contamination/ContaminationMod.java
@@ -7,14 +7,17 @@ import javax.annotation.Nullable;
 
 import com.teamacronymcoders.base.BaseModFoundation;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.capabilities.*;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.*;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -116,6 +119,33 @@ public class ContaminationMod extends BaseModFoundation<ContaminationMod> {
 	public ContaminationMod getInstance() {
 		return instance;
 	}
+	
+	@SubscribeEvent
+    public static void onUseItem(PlayerInteractEvent.RightClickBlock event)
+    {
+        if (!event.getWorld().isRemote) {
+            ItemStack stack = event.getEntityPlayer().getHeldItem(event.getHand());
+            int delta = 0;
+            IContaminationType type = null;
+            
+            if (stack.hasCapability(ContaminationMod.CONTAMINATION_INTERACTER_CAPABILITY, null)) {
+            	IContaminationInteracter interacter = stack.getCapability(ContaminationMod.CONTAMINATION_INTERACTER_CAPABILITY, null);
+            	type = interacter.getType();
+            	delta = interacter.getContaminationModifier();
+            }
+            else {
+            	return;
+            }
+
+            if(delta != 0) {
+            	int typePos = ContaminationTypeRegistry.getPosition(type);
+                Chunk chunk = event.getWorld().getChunk(event.getPos());
+            	IContaminationHolder pollution = chunk.getCapability(ContaminationMod.CONTAMINATION_HOLDER_CAPABILITY, null);
+            	pollution.set(typePos, pollution.get(typePos) + delta, true);
+            	event.getEntityPlayer().sendStatusMessage(new TextComponentString(type.getName() + " pollution: " + pollution.get(typePos)), true);
+            }
+        }
+    }
 	
 	@SubscribeEvent
 	public static void onServerTick(TickEvent.ServerTickEvent event) {
--- src/main/java/xyz/brassgoggledcoders/contamination/modules/smoke/ModuleSmoke.java
+++ src/main/java/xyz/brassgoggledcoders/contamination/modules/smoke/ModuleSmoke.java
@@ -9,11 +9,9 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -40,33 +38,6 @@ public class ModuleSmoke extends ModuleBase {
 			event.addCapability(new ResourceLocation(ContaminationMod.MODID, "contamination_interacter"), new ContaminationInteracterProvider(smoke, 1));
 		}
 	}
-	
-	@SubscribeEvent
-    public static void onUseItem(PlayerInteractEvent.RightClickBlock event)
-    {
-        if (!event.getWorld().isRemote) {
-            ItemStack stack = event.getEntityPlayer().getHeldItem(event.getHand());
-            int delta = 0;
-            IContaminationType type = null;
-            
-            if (stack.hasCapability(ContaminationMod.CONTAMINATION_INTERACTER_CAPABILITY, null)) {
-            	IContaminationInteracter interacter = stack.getCapability(ContaminationMod.CONTAMINATION_INTERACTER_CAPABILITY, null);
-            	type = interacter.getType();
-            	delta = interacter.getContaminationModifier();
-            }
-            else {
-            	return;
-            }
-
-            if(delta != 0) {
-            	int typePos = ContaminationTypeRegistry.getPosition(type);
-                Chunk chunk = event.getWorld().getChunk(event.getPos());
-            	IContaminationHolder pollution = chunk.getCapability(ContaminationMod.CONTAMINATION_HOLDER_CAPABILITY, null);
-            	pollution.set(typePos, pollution.get(typePos) + delta, true);
-            	event.getEntityPlayer().sendStatusMessage(new TextComponentString(type.getName() + " pollution: " + pollution.get(typePos)), true);
-            }
-        }
-    }
 	
 	@SubscribeEvent
 	public void onPlayerUpdate(LivingUpdateEvent event) {
