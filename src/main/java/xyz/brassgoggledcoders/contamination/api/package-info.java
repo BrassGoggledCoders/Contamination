42a8757ca3cb056b698c8076ce39f54bbbada625
warlordjones <warlordjones@gmail.com>
2019-03-19 16:30:37 +0000

    Create package-info

---
+++ src/main/java/xyz/brassgoggledcoders/contamination/api/package-info.java
@@ -0,0 +1,4 @@
+@API(owner = "contamination", apiVersion = "PRERELEASE", provides = "contamination|api")
+package xyz.brassgoggledcoders.contamination.api;
+
+import net.minecraftforge.fml.common.API;
