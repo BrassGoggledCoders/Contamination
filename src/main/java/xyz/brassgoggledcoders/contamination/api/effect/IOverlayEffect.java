ecce50b4fc98d38338b1d516d59c5c3ca1dbe34f
warlordjones <warlordjones@gmail.com>
2019-03-24 21:34:26 +0000

    Made the smoke overlay *way* less obnoxious

--- src/main/java/xyz/brassgoggledcoders/contamination/api/effect/IOverlayEffect.java
+++ src/main/java/xyz/brassgoggledcoders/contamination/api/effect/IOverlayEffect.java
@@ -3,8 +3,5 @@ package xyz.brassgoggledcoders.contamination.api.effect;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 
 public interface IOverlayEffect extends IContaminationEffect {
-	// N.B. Run in RenderGameOverlayEvent.Pre by default. If you want something
-	// else, you need a custom effect that you call from the event subscriber
-	// yourself
-	public void triggerEffect(RenderGameOverlayEvent event);
+	public void triggerEffect(RenderGameOverlayEvent.Pre event);
 }
--- src/main/java/xyz/brassgoggledcoders/contamination/effects/EffectOverlay.java
+++ src/main/java/xyz/brassgoggledcoders/contamination/effects/EffectOverlay.java
@@ -35,16 +35,19 @@ public class EffectOverlay implements IOverlayEffect {
 	}
 
 	@Override
-	public void triggerEffect(RenderGameOverlayEvent event) {
+	public void triggerEffect(RenderGameOverlayEvent.Pre event) {
+		if(!event.getType().equals(RenderGameOverlayEvent.ElementType.HELMET)) {
+			return;
+		}
 		Minecraft.getMinecraft().entityRenderer.setupOverlayRendering();
 		final int i = event.getResolution().getScaledWidth();
 		final int k = event.getResolution().getScaledHeight();
 		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glDisable(GL11.GL_DEPTH_TEST);
-		GL11.glDepthMask(false);
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		GL11.glDisable(GL11.GL_ALPHA_TEST);
+		//GL11.glDisable(GL11.GL_DEPTH_TEST);
+		//GL11.glDepthMask(false);
+		//GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		//GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		//GL11.glDisable(GL11.GL_ALPHA_TEST);
 		FMLClientHandler.instance().getClient().renderEngine.bindTexture(overlayLocation);
 		final Tessellator tessellator = Tessellator.getInstance();
 		tessellator.getBuffer().begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
@@ -53,9 +56,9 @@ public class EffectOverlay implements IOverlayEffect {
 		tessellator.getBuffer().pos(i / 2 + k, 0.0D, -90D).tex(1.0D, 0.0D).endVertex();
 		tessellator.getBuffer().pos(i / 2 - k, 0.0D, -90D).tex(0.0D, 0.0D).endVertex();
 		tessellator.draw();
-		GL11.glDepthMask(true);
-		GL11.glEnable(GL11.GL_DEPTH_TEST);
-		GL11.glEnable(GL11.GL_ALPHA_TEST);
-		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		//GL11.glDepthMask(true);
+		//GL11.glEnable(GL11.GL_DEPTH_TEST);
+		//GL11.glEnable(GL11.GL_ALPHA_TEST);
+		//GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 	}
 }
